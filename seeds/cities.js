acee01e53e81a585ee7d65849cdecb15377077ce
soma matsumoto <souma67jiji@icloud.com>
2025-08-14 01:35:59 +0900

    CRUD実装

---
+++ seeds/cities.js
@@ -0,0 +1,65 @@
+module.exports = [
+    {
+      "city": "東京市",
+      "latitude": 35.6897,
+      "longitude": 139.6922,
+      "country": "Japan",
+      "prefecture": "東京都",
+      "population": 37977000,
+      "population_proper": 13515271
+    },
+    {
+      "city": "大阪市",
+      "latitude": 34.75,
+      "longitude": 135.4601,
+      "country": "Japan",
+      "prefecture": "大阪府",
+      "population": 14977000,
+      "population_proper": 2725006
+    },
+    {
+      "city": "名古屋市",
+      "latitude": 35.1167,
+      "longitude": 136.9333,
+      "country": "Japan",
+      "prefecture": "愛知県",
+      "population": 9113000,
+      "population_proper": 2320361
+    },
+    {
+      "city": "横浜市",
+      "latitude": 35.4333,
+      "longitude": 139.6333,
+      "country": "Japan",
+      "prefecture": "神奈川県",
+      "population": 3757630,
+      "population_proper": 3757630
+    },
+    {
+      "city": "福岡市",
+      "latitude": 33.6,
+      "longitude": 130.4167,
+      "country": "Japan",
+      "prefecture": "福岡県",
+      "population": 2128000,
+      "population_proper": 1588924
+    },
+    {
+      "city": "札幌市",
+      "latitude": 43.0621,
+      "longitude": 141.3544,
+      "country": "Japan",
+      "prefecture": "北海道",
+      "population": 1961690,
+      "population_proper": 1961690
+    },
+    {
+      "city": "京都市",
+      "latitude": 35.0111,
+      "longitude": 135.7669,
+      "country": "Japan",
+      "prefecture": "京都府",
+      "population": 1805000,
+      "population_proper": 1459640
+    },
+];
